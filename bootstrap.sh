5562066ba8c6ea28699695576e28beaebb2fb237
Shane Barringer <shane.barringer@outlook.com>
2018-02-12 17:21:45 -0500

    updates setup info and adds command to install node

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,16 +11,30 @@ fi
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
+echo "Installing homebrew packages"
 brew tap homebrew/bundle
 brew bundle
 
 # Make ZSH the default shell environment
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp .zshrc.pre-oh-my-zsh .zshrc
+
+echo "Changing Shell to ZSH"
 chsh -s $(which zsh)
 
 # install current node versions
+echo "Installing nvm and latest version of node"
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install node
 nvm use node
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
+echo "Updating macOS settings"
 source .macos
