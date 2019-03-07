92b932161a7a403de026defc25775935b42f18c7
Shane Barringer <shane.barringer@outlook.com>
2019-03-07 12:26:21 -0600

    switches to static loading for plugins

---
+++ .zsh_plugins.sh
@@ -0,0 +1,29 @@
+source /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/osx/osx.plugin.zsh
+fpath+=( /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/osx )
+source /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/gitfast/gitfast.plugin.zsh
+fpath+=( /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/gitfast )
+source /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/jsontools/jsontools.plugin.zsh
+fpath+=( /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/jsontools )
+source /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/node/node.plugin.zsh
+fpath+=( /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/node )
+source /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/safe-paste/safe-paste.plugin.zsh
+fpath+=( /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/safe-paste )
+source /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/npm/npm.plugin.zsh
+fpath+=( /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/npm )
+source /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/brew/brew.plugin.zsh
+fpath+=( /Users/mxb5594/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/brew )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-bash/zsh-bash.plugin.zsh
+fpath+=( /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-bash )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-you-should-use/you-should-use.plugin.zsh
+fpath+=( /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-you-should-use )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/yadm-zsh/yadm.plugin.zsh
+fpath+=( /Users/mxb5594/.oh-my-zsh/custom/plugins/yadm-zsh )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /Users/mxb5594/.oh-my-zsh/custom/plugins/fast-syntax-highlighting )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-256color/zsh-256color.plugin.zsh
+fpath+=( /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-256color )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/cf/cf-zsh-autocomplete-plugin.plugin.zsh
+fpath+=( /Users/mxb5594/.oh-my-zsh/custom/plugins/cf )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-brew-services/zsh-brew-services.plugin.zsh
+fpath+=( /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-brew-services )
+source /Users/mxb5594/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
--- .zshrc
+++ .zshrc
@@ -8,13 +8,10 @@ ZSH_THEME="miloshadzic"
 COMPLETION_WAITING_DOTS="true"
 
 #### User configuration ####
-# set source
+# source oh-my-zsh config
 source $ZSH/oh-my-zsh.sh
 
 # Sets up antibody
-## Load antibody
-source <(antibody init)
-
 ## set antibody home
 ZSH="$(antibody home)/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh"
 
@@ -36,13 +33,9 @@ antibody bundle "
   ${ZSH_CUSTOM}/plugins/cf
   ${ZSH_CUSTOM}/plugins/zsh-brew-services
   ${ZSH_CUSTOM}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-"
+" > ~/.zsh_plugins.sh
 
-
-###### HUB CONFIG #######
-# Aliasing hub to git
-eval "$(hub alias -s)"
-###### END HUB CONFIG ######
+source ~/.zsh_plugins.sh
 
 # path to rbenv
 export PATH="/usr/local/sbin:$HOME/.rbenv/bin:$PATH"
@@ -72,15 +65,20 @@ export PATH="$PATH:/usr/local/opt/go/libexec/bin"
 # Exports `go/bin` in order to use go packages
 export PATH="$PATH:$GOPATH/bin"
 
+# updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mxb5594/Downloads/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/mxb5594/Downloads/google-cloud-sdk/path.zsh.inc'; fi
 
 # Aliases
+
+## Aliasing hub to git
+eval "$(hub alias -s)"
+
 ## GitHub personal
 alias gitmeHome='git config user.email shane.barringer@outlook.com'
 ## GitHub work
 alias gitmeWork='git config user.email shane_barringer@homedepot.com'
 
 ## npm
-
 ### npm ssl
 alias npm-ssl-true='npm config set strict-ssl true && yarn config set strict-ssl true'
 alias npm-ssl-false='npm config set strict-ssl false && yarn config set strict-ssl false'
@@ -90,6 +88,3 @@ alias npm-public='npm config set registry https://registry.npmjs.org && npm logi
 
 ### npm work
 alias npm-work='npm config set registry https://npm.artifactory.homedepot.com/artifactory/api/npm/npm/ && npm login && npm-ssl-false'
-
-# updates PATH for the Google Cloud SDK.
-if [ -f '/Users/mxb5594/Downloads/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/mxb5594/Downloads/google-cloud-sdk/path.zsh.inc'; fi
